e7fe7a6615bdd6970aec0523da55d0d75f143ec8
lbaf23 <2282480289@qq.com>
2020-10-23 20:57:18 +0800

    models sample

--- ResModel/models.py
+++ ResModel/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    CliEmail = models.CharField(max_length=50, primary_key=True)
+    CliName = models.CharField(max_length=50, null=True)
+
+class Search(models.Model):
+    CliEmail = models.ForeignKey('Client', to_field='CliEmail', on_delete=models.CASCADE)
+    SearchContent = models.CharField(max_length=200)
+    SearchTime = models.DateTimeField(auto_now_add=True)
+
