acef1548b3c78d478c40dfbe1bc7f65da5556602
lbaf23 <2282480289@qq.com>
2020-11-10 10:50:03 +0800

    Update Login.py

--- ResHub/controller/Login.py
+++ ResHub/controller/Login.py
@@ -1,7 +1,9 @@
 from ResModel.models import HubUser
+from django.http import HttpResponse, JsonResponse
 
 def identity_check(request):
     uid = request.POST.get('userId')
     pwd = request.POST.get('password')
     u = HubUser.objects.filter(UserEmail=uid).filter(UserPassword=pwd)
-    return u.__len__() > 0
+    res = u.__len__() > 0
+    return JsonResponse({'result': res})
