5f3a67e3700fd71734ac3e824c2a97137931ed0d
lyc200522 <63148721+lyc200522@users.noreply.github.com>
2020-11-18 11:01:11 +0800

    change Database

--- ResModel/models.py
+++ ResModel/models.py
@@ -5,39 +5,43 @@ from django.db import models
 class HubUser(models.Model):
     UserEmail = models.CharField(max_length=50, primary_key=True)
     UserName = models.CharField(max_length=50, null=True)
-    UserPassword = models.CharField(max_length=20,null=False)
-    UserImage = models.CharField(max_length=50,null=False)
+    UserPassword = models.CharField(max_length=20)
+    UserImage = models.CharField(max_length=50)
     UserIntroduction = models.CharField(max_length=2000,null=True)
 
 class Institution(models.Model):
-    InsName = models.CharField(max_length=50,null=False)
+    InsName = models.CharField(max_length=50)
     InsField = models.CharField(max_length=50,null=True)
-    InsIntroduction = models.CharField(max_length=50,null=False)
+    InsIntroduction = models.CharField(max_length=50)
 
 class Researcher(models.Model):
-    ResId = models.CharField(max_length=50,null=False)
-    IsClaim = models.BooleanField(null=False)
+    ResId = models.CharField(max_length=50)
+    IsClaim = models.BooleanField()
     UserEmail = models.OneToOneField('HubUser',to_field='UserEmail',on_delete=models.CASCADE,null=True)
-    ResName = models.CharField(max_length=50,null=False)
-    NormalizedName = models.CharField(max_length=50,null=False)
+    ResName = models.CharField(max_length=50)
+    NormalizedName = models.CharField(max_length=50,null=True)
     ResEmail = models.CharField(max_length=50,null=True,unique=True)
     ResField = models.CharField(max_length=50,null=True)
     ResCompany = models.ForeignKey('Institution',to_field='id',on_delete=models.CASCADE,null=True)
     ResIntroduction = models.CharField(max_length=50,null=True)
-    LiteratureNum = models.IntegerField(null=False)
-    CitedNum = models.IntegerField(null=False)
-    VisitNum = models.IntegerField(null=False)
-    ConcernNum = models.IntegerField(null=False)
+    LiteratureNum = models.IntegerField(default=0)
+    CitedNum = models.IntegerField(null=True)
+    VisitNum = models.IntegerField(default=0)
+    ConcernNum = models.IntegerField(default=0)
     ResHIndex = models.IntegerField(null=True)
 
-class Paper(models.Model):
-    PaperId = models.CharField(max_length=50, primary_key=True)
-    PaperTitle = models.CharField(max_length=200,null=False)
-    ReadNum = models.IntegerField(auto_created=0)
-    LinkNum = models.CharField(max_length=200,auto_created=0)
-    CollectionNum = models.IntegerField(auto_created=0)
-    IsUserUpload = models.BooleanField(null=False,auto_created=False)
+class Literature(models.Model):
+    LitId = models.CharField(max_length=50,primary_key=True)
+    LitTitle = models.CharField(max_length=200)
+    ReadNum = models.IntegerField(default=0)
+    LinkNum = models.CharField(max_length=200,default=0)
+    CollectionNum = models.IntegerField(default=0)
+    IsUserUpload = models.BooleanField(default=False)
     PaperAuthor = models.ManyToManyField(to="Researcher",null=True)
+    class Meta:
+        abstract = True
+
+class Paper(Literature):
     PaperVenue =  models.CharField(max_length=200,null=False)
     PaperTime = models.DateField(auto_now_add=False,null=True)
     PaperCitation = models.IntegerField(null=False)
@@ -48,25 +52,13 @@ class Paper(models.Model):
     PaperIssue = models.CharField(max_length=50,null=True)
     PaperPublisher = models.CharField(max_length=50,null=True)
     PaperType = models.CharField(max_length=50,null=True)
-    LitType = models.IntegerField(auto_created=1)
-class Patent(models.Model):
-    PatentId = models.CharField(max_length=50, primary_key=True)
-    PatentTitle = models.CharField(max_length=200,null=False)
-    ReadNum = models.IntegerField(auto_created=0)
-    LinkNum = models.CharField(max_length=200,auto_created=0)
-    CollectionNum = models.IntegerField(auto_created=0)
-    IsUserUpload = models.BooleanField(null=False,auto_created=False)
-    PatentAuthor = models.ManyToManyField(to="Researcher",null=True)
+    LitType = models.IntegerField(default=1)
+
+class Patent(Literature):
     PatentAbstract = models.CharField(max_length=2000,null=True)
-    LitType = models.IntegerField(auto_created=2)
-class Project(models.Model):
-    ProjectId = models.CharField(max_length=50, primary_key=True)
-    ProjectTitle = models.CharField(max_length=200,null=False)
-    ReadNum = models.IntegerField(auto_created=0)
-    LinkNum = models.CharField(max_length=200,auto_created=0)
-    CollectionNum = models.IntegerField(auto_created=0)
-    IsUserUpload = models.BooleanField(null=False,auto_created=False)
-    ProjectAuthor = models.ManyToManyField(to="Researcher",null=True)
+    LitType = models.IntegerField(default=2)
+
+class Project(Literature):
     GrantYear = models.DateField(auto_now_add=False,null=True)
     Subject = models.CharField(max_length=100,null=True)
     ProjectLeader = models.CharField(max_length=50,null=True)
@@ -77,7 +69,8 @@ class Project(models.Model):
     StudyPeriod = models.CharField(max_length=100,null=True)
     SubjectHeadingCN = models.CharField(max_length=100,null=True)
     SubjectHeadingEN = models.CharField(max_length=200,null=True)
-    LitType = models.IntegerField(auto_created=3)
+    LitType = models.IntegerField(default=3)
+
 class Concern(models.Model):
     UserEmail = models.ForeignKey('HubUser',to_field='UserEmail',on_delete=models.CASCADE)
     ResearchId = models.ForeignKey('Researcher',to_field='id',on_delete=models.CASCADE)
@@ -85,9 +78,9 @@ class Concern(models.Model):
 
 class Collection(models.Model):
     UserEmail = models.ForeignKey('HubUser',to_field='UserEmail',on_delete=models.CASCADE)
-    PaperId = models.ForeignKey('Paper',to_field='PaperId',on_delete=models.CASCADE,null=True)
-    PatentId = models.ForeignKey('Patent',to_field='PatentId',on_delete=models.CASCADE,null=True)
-    ProjectId = models.ForeignKey('Project',to_field='ProjectId',on_delete=models.CASCADE,null=True)
+    PaperId = models.ForeignKey('Paper',to_field='LitId',on_delete=models.CASCADE,null=True)
+    PatentId = models.ForeignKey('Patent',to_field='LitId',on_delete=models.CASCADE,null=True)
+    ProjectId = models.ForeignKey('Project',to_field='LitId',on_delete=models.CASCADE,null=True)
     CollectionTime = models.DateTimeField(auto_now_add=True)
 
 class Mail(models.Model):
@@ -100,8 +93,8 @@ class Mail(models.Model):
 
 class Message(models.Model):
     ReceiveEmail = models.ForeignKey('HubUser',to_field='UserEmail',on_delete=models.CASCADE)
-    MessageContent = models.CharField(max_length=2000,null=False)
-    MessageType = models.SmallIntegerField(null=False)
+    MessageContent = models.CharField(max_length=2000)
+    MessageType = models.SmallIntegerField()
     SendTime = models.DateTimeField(auto_now_add=True)
     IsRead = models.BooleanField(default=True)
 
@@ -113,39 +106,39 @@ class ChatFriends(models.Model):
     unread = models.IntegerField(default=0)
 
 class Administrators(models.Model):
-    AdmEmail = models.CharField(max_length=50,null=False)
-    AdmPassword = models.CharField(max_length=20,null=False)
+    AdmEmail = models.CharField(max_length=50)
+    AdmPassword = models.CharField(max_length=20)
 
 class Browse(models.Model):
     UserEmail = models.ForeignKey('HubUser',to_field='UserEmail',on_delete=models.CASCADE)
-    PaperId = models.ForeignKey('Paper',to_field='PaperId',on_delete=models.CASCADE,null=True)
-    PatentId = models.ForeignKey('Patent',to_field='PatentId',on_delete=models.CASCADE,null=True)
-    ProjectId = models.ForeignKey('Project',to_field='ProjectId',on_delete=models.CASCADE,null=True)
+    PaperId = models.ForeignKey('Paper',to_field='LitId',on_delete=models.CASCADE,null=True)
+    PatentId = models.ForeignKey('Patent',to_field='LitId',on_delete=models.CASCADE,null=True)
+    ProjectId = models.ForeignKey('Project',to_field='LitId',on_delete=models.CASCADE,null=True)
     ResearchId = models.ForeignKey('Researcher',to_field='id',on_delete=models.CASCADE,null=True)
-    BrowseType = models.SmallIntegerField(null=False)
+    BrowseType = models.SmallIntegerField()
     BrowseTime = models.DateTimeField(auto_now_add=True)
 
 class Search(models.Model):
     UserEmail = models.ForeignKey('HubUser', to_field='UserEmail', on_delete=models.CASCADE)
-    SearchContent = models.CharField(max_length=200,null=False)
+    SearchContent = models.CharField(max_length=200)
     SearchTime = models.DateTimeField(auto_now_add=True)
 
 class Appeal(models.Model):
     ResearchId = models.ForeignKey('Researcher',to_field='id',on_delete=models.CASCADE)
     UserEmail = models.ForeignKey('HubUser',to_field='UserEmail',on_delete=models.CASCADE)
-    AppealState = models.BooleanField(null=False)
+    AppealState = models.BooleanField()
     AppealTime = models.DateTimeField(auto_now_add=True)
 
 class Review(models.Model):
     UserEmail = models.ForeignKey('HubUser',to_field='UserEmail',on_delete=models.CASCADE)
     UploadTime = models.DateTimeField(auto_now_add=True)
-    ReviewState = models.BooleanField(null=False)
+    ReviewState = models.BooleanField()
     ReviewTime = models.DateTimeField(auto_now_add=False)
 
 class Relationship(models.Model):
     ResearchId1 = models.ForeignKey('Researcher',related_name='first',to_field='id',on_delete=models.CASCADE)
     ResearchId2 = models.ForeignKey('Researcher',related_name='second',to_field='id',on_delete=models.CASCADE)
-    LiteratureNum = models.IntegerField(null=False)
+    LiteratureNum = models.IntegerField()
 
 
 
