403a11690806c6cc4a424aea3ef8743e2ecccc2d
Matrix.L <51694046+Matrix-Lee@users.noreply.github.com>
2020-12-17 20:57:31 +0800

    个人门户

--- ResHub/personal_portal/getPersonalPortal.py
+++ ResHub/personal_portal/getPersonalPortal.py
@@ -4,25 +4,25 @@ from ResModel.models import HubUser, Researcher, Institution, Relationship, Conc
 
 def getPersonalPortal(request):
     try:
-        res = {}
         if request.method == "GET":
             userId = request.GET.get('userId')
             resId = request.GET.get('resId')
             if ((userId is not None) and (resId is not None)):
-                # temp2记录从Researcher得到的结果
-                temp2 = Researcher.objects.filter(ResEmail=resId).first()
 
-                if(temp2 is not None):
-                    res['realName'] = temp2.ResName
-                    res['ResField'] = temp2.ResField
-                    res['InsName'] = temp2.ResCompany.InsName
-                    res['ResCompany'] = temp2.ResCompany.id
-                    res['ResEmail'] = temp2.ResEmail
-                    res['CitedNum'] = temp2.CitedNum
-                    res['literatureNum'] = temp2.LiteratureNum
-                    res['visitnum'] = temp2.VisitNum
+                researcher = Researcher.objects.filter(ResEmail=resId).first()
+                hubuser = HubUser.objects.filter(UserEmail=userId).first()
+                if((researcher is not None) and (hubuser is not None)):
+                    res = {}
+                    res['realName'] = researcher.ResName
+                    res['isClaimed'] = researcher.IsClaim
+                    res['InsName'] = researcher.ResCompany.InsName
+                    res['ResCompany'] = researcher.ResCompany.id
+                    res['ResEmail'] = researcher.ResEmail
+                    res['CitedNum'] = researcher.CitedNum
+                    res['literatureNum'] = researcher.LiteratureNum
+                    res['visitnum'] = researcher.VisitNum
                     coopList = []
-                    temp = temp2.ResId
+                    temp = researcher.ResId
                     temp3 = Relationship.objects.filter(ResearchId1=temp).all()
                     temp4 = Relationship.objects.filter(ResearchId2=temp).all()
                     len = 0
@@ -40,7 +40,7 @@ def getPersonalPortal(request):
                         len = len+1
                     res['coopList'] = coopList
 
-                    if(temp2.IsClaim == False):
+                    if(researcher.IsClaim == False):
                         res['IsClaim'] = False
                     else:
                         res['IsClaim'] = True
@@ -50,13 +50,13 @@ def getPersonalPortal(request):
                         res['isFollowing'] = True
                     else:
                         res['isFollowing'] = False
-                    if(temp2.UserEmail == userId):
+                    if(researcher.UserEmail == userId):
                         res['myPortal'] = True
                     else:
                         res['myPortal'] = False
-                    res['visitNum'] = temp2.VisitNum
+                    res['visitNum'] = researcher.VisitNum
                     res['followNum'] = Concern.objects.filter(
-                        ResearchId=temp2.ResId).all().__len__()
+                        ResearchId=researcher.ResId).all().__len__()
                     return JsonResponse({
                         "status": 1,
                         "message": res
