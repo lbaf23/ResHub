af2c311d04af761158380b8f3b62a6e22ebea465
xiangjie3291 <985812032@qq.com>
2020-12-19 21:16:06 +0800

    更换头像、返回审核申诉列表

---
+++ ResHub/user/changeHead.py
@@ -0,0 +1,29 @@
+import json
+
+from django.http import HttpResponse, JsonResponse
+from ResModel.models import HubUser
+
+
+def changeHead(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        userEmail = data.get("userEmail")
+        userHead = data.get("userHead")
+        if userEmail is not None and userHead is not None:
+            user = HubUser.objects.filter(UserEmail=userEmail).first()
+            user.UserImage = userHead
+            user.save()
+            return JsonResponse({
+                "status": 1,
+                "message": "更换头像成功",
+            }, safe=False)
+        else:
+            return JsonResponse({
+                "status": 2,
+                "message": "请求参数错误"
+            })
+    else:
+        return JsonResponse({
+          "status": 3,
+          "message": "请求方法错误"
+        })
