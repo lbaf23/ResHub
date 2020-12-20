26eff395073021359d10a70c8dcc8178ab38bb17
lyc200522 <63148721+lyc200522@users.noreply.github.com>
2020-12-20 17:04:58 +0800

    lyc

--- ResHub/controller/searchHistory.py
+++ ResHub/controller/searchHistory.py
@@ -5,8 +5,8 @@ from django.http import JsonResponse
 from ResModel.models import Search
 
 def add_search_history(request):
-    user_id = request.GET.get('userId')
-    keyword = request.GET.get('keyWords')
+    user_id = request.POST.get('userId')
+    keyword = request.POST.get('keyWords')
     succeed = True
     u = Search(UserEmail=user_id,SearchContent=keyword)
     u.save()
