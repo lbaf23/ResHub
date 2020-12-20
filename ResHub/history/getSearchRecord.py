350b18a3c22c00c06804eda3b84aa3172e726f97
Matrix.L <51694046+Matrix-Lee@users.noreply.github.com>
2020-12-20 21:30:04 +0800

    提交一次

--- ResHub/history/getSearchRecord.py
+++ ResHub/history/getSearchRecord.py
@@ -9,36 +9,27 @@ def getSearchRecord(request):
             if userId is not None:
                 res = {}
                 list = []
-                search = Search.objects.filter(UserEmail=userId)
+                search = Search.objects.filter(UserEmail_id=userId)
                 len = search.__len__()
                 if(len == 0):
-                    return JsonResponse({
-                        "status": 0,
-                        "message": "Nothing"
-                    })
+                    res['list'] = []
+                    return JsonResponse(res)
                 for i in search:
                     temp = {}
                     temp['SearchContent'] = i.SearchContent
                     temp['SearchTime'] = str(i.SearchTime)
+                    temp['id'] = i.id
                     list.append(temp)
                 res['len'] = search.__len__()
                 res['list'] = list
-                return JsonResponse({
-                    "status": 1,
-                    "message": res
-                })
+                print(res['list'])
+                return JsonResponse(res)
             else:
-                return JsonResponse({
-                    "status": 2,
-                    "message": "请求参数错误"
-                })
+                res['list'] = []
+                return JsonResponse(res)
         else:
-            return JsonResponse({
-                "status": 3,
-                "message": "请求方法错误"
-            })
+            res['list'] = []
+            return JsonResponse(res)
     except Exception as e:
-        return JsonResponse({
-            "status": 4,
-            "message": str(e)
-        })
+        res['list'] = []
+        return JsonResponse(res)
