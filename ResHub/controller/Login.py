e11065bdab7d146c9564da9035f731fb2daa36fb
lyc200522 <63148721+lyc200522@users.noreply.github.com>
2020-12-22 20:55:38 +0800

    1

--- ResHub/controller/Login.py
+++ ResHub/controller/Login.py
@@ -43,7 +43,7 @@ def register(request):
     result = True
     u=HubUser.objects.filter(UserEmail=address)
     if(len(u)==0 and address!=None):
-        user = HubUser(UserEmail=address,UserIntroduction=description,UserPassword=pwd,UserName=name)
+        user = HubUser(UserEmail=address,UserIntroduction=description,UserPassword=pwd,UserName=name,UserImage="head01.png")
         user.save()
     else:
         result=False
