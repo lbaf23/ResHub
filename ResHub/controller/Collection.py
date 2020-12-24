07dd6f813382d839aed5a6f7d00ebf6bf6189f2c
lyc200522 <63148721+lyc200522@users.noreply.github.com>
2020-12-24 11:30:11 +0800

    1

--- ResHub/controller/Collection.py
+++ ResHub/controller/Collection.py
@@ -41,12 +41,11 @@ def get_collection(request):
             }
         elif t==3:
             project = c[i].ProjectId
-            li=list()
-            li.append(''if Project.ProjectLeader is None else Project.ProjectLeader)
             j = {
             'paperId': project.ProjectId,
             'title': project.ProjectTitle,
             'msg': ''if project.ZhAbstract is None else project.ZhAbstract,
+            'author':''if Project.ProjectLeader is None else Project.ProjectLeader.split(','),
             'type':  str(t),
             'collectionSum':project.CollectionNum,
             'viewSum':project.ReadNum,
